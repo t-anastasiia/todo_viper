51e833ca0c819823533acb808ef15fcafbfaffd3
t-anastasiia <nastaytalmazan@gmail.com>
2025-02-01 15:02:54 +1000

    initial commit

---
+++ todo_viper/todo_viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo_viper
+//
+//  Created by Анастасия Талмазан on 2025-02-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
